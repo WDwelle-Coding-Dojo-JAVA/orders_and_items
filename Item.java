588b492c8b3cb4d21e228ae817b6f26dc9a9f4e5
Will Dwelle <willdwelle@frontier.com>
2021-11-30 14:30:30 -0800

    Baristas challenge initial

--- Item.java
+++ Item.java
@@ -1,4 +1,25 @@
 public class Item{
-    public String name;
-    public double price;
+    private String name;
+    private double price;
+//---constructors---
+    public Item(String name, double price){
+        this.name = name;
+        this.price = price;
+    }
+//---Setters and Getters
+    public void setName(String newName){
+        name = newName;
+    }
+
+    public void setPrice(double newPrice){
+        price = newPrice;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public double getPrice(){
+        return price;
+    }
 }
--- Order.java
+++ Order.java
@@ -1,15 +1,60 @@
 import java.util.*;
 public class Order{
-    public String name;
-    public double total;
-    public boolean ready;
-    public ArrayList<Item> items = new ArrayList();
+    private String name;
+    //public double total;
+    private boolean ready;
+    private ArrayList<Item> items = new ArrayList();
 
-    public double getTotal(){
-        for(Item item : items){
-            total = total + item.price;
-            System.out.println(total);
-        }
-        return total;
+    //public double getTotal(){
+        //for(Item item : items){
+            //total = total + item.price;
+            //System.out.println(total);
+        //}
+        //return total;
+    //}
+    //---Constuctors---
+    public Order(){
+        this.name = "Guest";
+        this.ArrayList<Item> = new ArrayList();
+    }
+
+    public Order(String name){
+        this.name = name;
+        this.ArrayList<Item> = new ArrayList();
+    }
+
+
+//---Methods---
+    public void addItem(Item){
+
+    }
+
+    public void getStatusMessage(){
+
+    }
+    public void getOrderTotal(){
+
+    }
+
+    public void display(Order){
+        System.out.println(this.name);
+        System.out.println(this.items.price);
+        System.out.println(getOrderTotal);
+    }
+    //---Setters and Getters---
+    public void setGuestName(String guestName){
+        name = guestName;
+    }
+    
+    public void setOrderReady(boolean orderReady){
+        ready = orederReady;
+    }
+
+    public String getGuest(){
+        return name;
+    }
+
+    public boolean getReady(){
+        return ready;
     }
 }
